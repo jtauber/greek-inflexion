d9a79787963efadbf54c6e1df22690b88194db74
James Tauber <jtauber@jtauber.com>
2016-11-30 23:07:11 -0500

    cleaned up more duplicate code

--- morphgnt_generate.py
+++ morphgnt_generate.py
@@ -5,6 +5,8 @@ from pysblgnt import morphgnt_rows
 from accent import strip_length  # , rebreath
 from greek_inflexion import GreekInflexion
 from morphgnt_utils import bcv_tuple, convert_parse
+from test_generate import output_item
+
 
 ginflexion = GreekInflexion("stemming.yaml", "morphgnt_johannine_lexicon.yaml")
 
@@ -43,13 +45,6 @@ for book_num in [4, 23, 24, 25]:
                     stem for key, stem in
                     ginflexion.possible_stems(form, "^" + key + "$")]
 
-            # if [strip_length(w) for w in sorted(generated)] == \
-            #         [strip_length(w) for w in sorted(form.split("/"))]:
-            #     correct = "✓"
-            # else:
-            #     correct = "✕"
-            #     incorrect_count += 1
-
             if strip_length(form) in [
                     strip_length(w) for w in sorted(generated)]:
                 correct = "✓"
@@ -58,60 +53,8 @@ for book_num in [4, 23, 24, 25]:
                 incorrect_count += 1
 
             if debug or correct == "✕":
-                print("-")
-                print("    lemma: {}".format(lemma))
-                print("    key: {}".format(key))
-                print("    form: {}".format(form))
-                if stem:
-                    if len(stem) == 1:
-                        print("    stem: {}".format(list(stem)[0]))
-                    else:
-                        print("    stem: {}".format(list(stem)))
-                if stem_guess:
-                    print("    stem_guess: {}".format(stem_guess))
-                print("    correct: \"[{}/{}{}]\"".format(
-                    len(generated), c, correct))
-                print("    generated:")
-                for generated_form, details in generated.items():
-                    print("      -")
-                    print("        form: {}".format(generated_form))
-                    print("        details:")
-                    for detail in details:
-                        print("          -")
-                        if "stem" in detail:
-                            print("            stem: {}".format(detail["stem"]))
-                        if "stemming" in detail:
-                            print("            stemming:")
-                            print("                base: {}".format(detail["stemming"]["base"]))
-                            print("                ending: {}".format(detail["stemming"]["ending"]))
-                            print("                rule: \"{0.a}|{0.b}>{0.c}<{0.d}|{0.e}\"".format(detail["stemming"]["rule"]))
-                            print("                used_default: {}".format(detail["stemming"]["used_default"]))
-                        if "original_form" in detail:
-                            print("            original_form: {}".format(detail["original_form"]))
-                        if "override" in detail:
-                            print("            override: {}".format(detail["override"]))
+                output_item(
+                    lemma, key, form,
+                    stem, stem_guess, generated, correct)
 
 print("{}/{} incorrect".format(incorrect_count, total_count))
-
-            # c = form.count("/") + 1
-            # stem = ginflexion.find_stems(lemma, key, tags)
-            # generated = ginflexion.generate(lemma, key, tags)
-            #
-            # for generated_form, details in generated.items():
-            #     if form == generated_form:
-            #         for i, detail in enumerate(details):
-            #
-            #             print("{:12s}".format(lemma), end="\t")
-            #             print("{:8s}".format(key), end="\t")
-            #             print("{:16s}".format(form), end="\t")
-            #             print("{:24s}".format("/".join(stem)), end="\t")
-            #
-            #             print("{:2d}".format(i), end="\t")
-            #             print("{:24s}".format(rebreath(detail.get("stem", "-"))), end="\t")
-            #
-            #             if "stemming" in detail:
-            #                 print("{:12s}".format(rebreath(detail["stemming"]["base"]) or "-"), end="\t")
-            #                 print("{:8s}".format(detail["stemming"]["ending"] or "-"), end="\t")
-            #                 print("{0.a}|{0.b}>{0.c}<{0.d}|{0.e}".format(detail["stemming"]["rule"]))
-            #             else:
-            #                 print()
