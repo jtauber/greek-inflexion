5752d4a135d021cb692b83a4802003750e00c7ac
James Tauber <jtauber@jtauber.com>
2019-07-17 20:35:23 -0400

    updated lexicon based on new stemming rules

--- homer_generate.py
+++ homer_generate.py
@@ -45,6 +45,7 @@ with open(FILENAME) as f:
             "no-final-nu-yai.3s",
             "no-final-nu-aps.3p",
             "no-final-nu-pai.3s",
+            "no-final-nu-aas.3p",
             "alt-apo-pl",
             "Homer",
         ])
